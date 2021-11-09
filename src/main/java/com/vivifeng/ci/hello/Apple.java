e189a09f877a6f9684b7fa77aed65422b550b0b3
liufeng <liufeng@126.com>
2021-11-09 20:41:44 +0800

    aaa

--- src/main/java/com/vivifeng/ci/hello/Apple.java
+++ src/main/java/com/vivifeng/ci/hello/Apple.java
@@ -2,7 +2,7 @@ package com.vivifeng.ci.hello;
 
 public class Apple {
 	
-	private String appName = "good apple |||AAAA BBB CCC test my jenkins by liufeng";
+	private String appName = "good apple |||AAAA BBB CCC DDDDDDDDDDDDD test my jenkins by liufeng";
 
 	public String getAppName() {
 		return appName;
